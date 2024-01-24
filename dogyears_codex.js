b79ec904719e8478797a0d1fd64a61179cb034c0
Beendeta <88084190+Beendeta@users.noreply.github.com>
2024-01-24 11:52:03 +0800

    dogyears_codex.js

--- dogyears_codex.js
+++ dogyears_codex.js
@@ -1,3 +1,5 @@
-function cyears (form) {
-    form.dogage.value = form.myage.value * 7;
-}
+function calculateDogAge() {
+            var myAge = document.getElementById("myage").value;
+            var dogAge = myAge * 7;
+            document.getElementById("dogage").innerText = dogAge;
+        }
